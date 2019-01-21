24b1e3181e1877bd73f14dc7625936902039b2c6
cappydev <kevin.lanel@wanadoo.fr>
2019-01-21 10:46:47 +0100

    exo2js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var lastName = 'joe';
+var firstName = 'John';
+var city = 'New-York';
+alert("Nom : " + lastName + "\nPrénom : " +firstName +"\nVille : " + city);
+"Hello there " + name + ". Welcome."
